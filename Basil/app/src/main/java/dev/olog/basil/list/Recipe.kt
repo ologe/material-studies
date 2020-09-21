e98ad3d243f28045449e67a897b44540ff39cad3
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-21 21:08:43 +0200

    created Recipe and sample data

---
+++ Basil/app/src/main/java/dev/olog/basil/list/Recipe.kt
@@ -0,0 +1,19 @@
+package dev.olog.basil.list
+
+data class Recipe(
+    val url: String,
+    val title: String
+) {
+
+    companion object {
+
+        val sample = listOf(
+            Recipe("https://loremflickr.com/500/500", "Creamy Pesto Pasta"),
+            Recipe("https://loremflickr.com/500/500", "Beef Pot Pie"),
+            Recipe("https://loremflickr.com/500/500", "Herb Roasted Chicken"),
+            Recipe("https://loremflickr.com/500/500", "Spinach File Puffs"),
+        )
+
+    }
+
+}
