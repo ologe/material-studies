0f088db2025317ffa93c5f8dd213bfd731829ce6
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-21 22:04:45 +0200

    added modifier to background

--- Basil/app/src/main/java/dev/olog/basil/composable/Background.kt
+++ Basil/app/src/main/java/dev/olog/basil/composable/Background.kt
@@ -3,10 +3,15 @@ package dev.olog.basil.composable
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun Background(content: @Composable () -> Unit) {
+fun Background(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
     Surface(
+        modifier = modifier,
         color = MaterialTheme.colors.background,
         content = content
     )
