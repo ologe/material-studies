5fdb45e115383ba0d976574b251a2caad6aa4292
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-21 23:10:43 +0200

    added app name in drawer

--- Basil/app/src/main/java/dev/olog/basil/drawer/DrawerContent.kt
+++ Basil/app/src/main/java/dev/olog/basil/drawer/DrawerContent.kt
@@ -1,8 +1,24 @@
 package dev.olog.basil.drawer
 
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Stack
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import dev.olog.basil.R
+import java.util.*
 
 @Composable
 fun DrawerContent() {
-
+    Stack(Modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(R.string.app_name).toUpperCase(Locale.ROOT), // TODO use widget uppercase when avaialble
+            modifier = Modifier.align(Alignment.BottomCenter),
+            style = MaterialTheme.typography.h2,
+            // TODO green
+        )
+    }
 }
