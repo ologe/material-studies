a5da5fad701b75f826bd4821a961bd75d063d770
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-22 19:21:25 +0200

    added explicit font size to typography

--- Basil/app/src/main/java/dev/olog/basil/theme/Type.kt
+++ Basil/app/src/main/java/dev/olog/basil/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.font
 import androidx.compose.ui.text.font.fontFamily
+import androidx.compose.ui.unit.sp
 import dev.olog.basil.R
 
 private val montserrat = fontFamily(
@@ -21,55 +22,68 @@ private val lekton = fontFamily(
 val typography = typographyFromDefaults(
     h1 = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 96.sp,
     ),
     h2 = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 60.sp
     ),
     h3 = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 48.sp
     ),
     h4 = TextStyle(
         fontFamily = montserrat,
         fontWeight = FontWeight.SemiBold,
+        fontSize = 34.sp
     ),
     h5 = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp
     ),
     h6 = TextStyle(
         fontFamily = lekton,
         fontWeight = FontWeight.Bold,
+        fontSize = 21.sp
     ),
     subtitle1 = TextStyle(
         fontFamily = lekton,
         fontWeight = FontWeight.Bold,
+        fontSize = 17.sp
     ),
     subtitle2 = TextStyle(
         fontFamily = lekton,
         fontWeight = FontWeight.Bold,
+        fontSize = 15.sp
     ),
     body1 = TextStyle(
         fontFamily = montserrat,
         fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
     ),
     body2 = TextStyle(
         fontFamily = montserrat,
         fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     ),
     button = TextStyle(
         fontFamily = montserrat,
         fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     ),
     caption = TextStyle(
         fontFamily = montserrat,
         fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
     ),
     overline = TextStyle(
         fontFamily = montserrat,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp
     )
 )
 
