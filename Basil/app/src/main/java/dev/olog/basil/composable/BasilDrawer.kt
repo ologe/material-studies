0f1cbb57f5e599425ea8e15e286b453791de2788
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-24 00:36:26 +0200

    fixed swipe bug

--- Basil/app/src/main/java/dev/olog/basil/composable/BasilDrawer.kt
+++ Basil/app/src/main/java/dev/olog/basil/composable/BasilDrawer.kt
@@ -52,7 +52,7 @@ fun BasilDrawer(
             anchors = anchors,
             orientation = Orientation.Vertical,
             thresholds = { _, _ ->
-                FractionalThreshold(0.3f)
+                FractionalThreshold(0.5f)
             }
         ).offsetGetter(y = { state.drawerOffset })
     ) {
