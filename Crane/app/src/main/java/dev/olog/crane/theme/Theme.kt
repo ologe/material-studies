f2ab48aeb467c57c81b43844ab7eebac67226f1e
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-26 19:07:35 +0200

    added crane theming

--- Crane/app/src/main/java/dev/olog/crane/theme/Theme.kt
+++ Crane/app/src/main/java/dev/olog/crane/theme/Theme.kt
@@ -1,42 +1,38 @@
 package dev.olog.crane.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
-)
+val purple800 = Color(0xFF_5d1049)
+val purple900 = Color(0xFF_4e0d3a)
+val purple700 = Color(0xFF_720d5d)
+val red700 = Color(0xFF_e30424)
 
-private val LightColorPalette = lightColors(
-    primary = purple500,
-    primaryVariant = purple700,
-    secondary = teal200
+private val Palette = lightColors(
+    background = purple800,
+    onBackground = Color.White,
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    primary = purple700,
+    primaryVariant = purple900,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
+
+    secondary = red700,
+    secondaryVariant = red700,
+    onSecondary = Color.White,
+
+    surface = Color.White,
     onSurface = Color.Black,
-    */
+
+    error = Color(0xFF_fd9725),
+    onError = Color.White
 )
 
 @Composable
-fun CraneTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun CraneTheme(content: @Composable() () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = Palette,
         typography = typography,
         shapes = shapes,
         content = content
--- Crane/app/src/main/java/dev/olog/crane/theme/Type.kt
+++ Crane/app/src/main/java/dev/olog/crane/theme/Type.kt
@@ -2,27 +2,116 @@ package dev.olog.crane.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.font
+import androidx.compose.ui.text.font.fontFamily
 import androidx.compose.ui.unit.sp
+import dev.olog.crane.R
 
-// Set of Material typography styles to start with
-val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+val raleway = fontFamily(
+    font(R.font.raleway_light, FontWeight.Light),
+    font(R.font.raleway_regular, FontWeight.Normal),
+    font(R.font.raleway_semibold, FontWeight.SemiBold),
+    font(R.font.raleway_medium, FontWeight.Medium),
+)
+
+val typography = typographyFromDefaults(
+    h1 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.Light,
+        fontSize = 96.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = raleway,
         fontWeight = FontWeight.Normal,
+        fontSize = 60.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 48.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 34.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body1 = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = raleway,
         fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+    ),
+    caption = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = raleway,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp
     )
-    */
 )
+
+private fun typographyFromDefaults(
+    h1: TextStyle,
+    h2: TextStyle,
+    h3: TextStyle,
+    h4: TextStyle,
+    h5: TextStyle,
+    h6: TextStyle,
+    subtitle1: TextStyle,
+    subtitle2: TextStyle,
+    body1: TextStyle,
+    body2: TextStyle,
+    button: TextStyle,
+    caption: TextStyle,
+    overline: TextStyle
+): Typography {
+    val defaults = Typography()
+    return Typography(
+        h1 = defaults.h1.merge(h1),
+        h2 = defaults.h2.merge(h2),
+        h3 = defaults.h3.merge(h3),
+        h4 = defaults.h4.merge(h4),
+        h5 = defaults.h5.merge(h5),
+        h6 = defaults.h6.merge(h6),
+        subtitle1 = defaults.subtitle1.merge(subtitle1),
+        subtitle2 = defaults.subtitle2.merge(subtitle2),
+        body1 = defaults.body1.merge(body1),
+        body2 = defaults.body2.merge(body2),
+        button = defaults.button.merge(button),
+        caption = defaults.caption.merge(caption),
+        overline = defaults.overline.merge(overline)
+    )
+}
