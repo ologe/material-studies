b472b9b4d8645675c96d6974355ce0ed0e4a2391
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-09-27 15:43:44 +0200

    created fortnightly project

---
+++ Fortnightly/app/src/main/java/dev/olog/fortnightly/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.olog.fortnightly
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.setContent
+import dev.olog.fortnightly.ui.FortnightlyTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FortnightlyTheme {
+
+            }
+        }
+    }
+}
