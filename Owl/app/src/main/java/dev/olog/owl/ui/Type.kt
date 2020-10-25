c33a0f21c861b1a980a4af8af70f4a019538476b
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-10-26 00:50:14 +0100

    from TypographyUtils from :shared

--- Owl/app/src/main/java/dev/olog/owl/ui/Type.kt
+++ Owl/app/src/main/java/dev/olog/owl/ui/Type.kt
@@ -1,12 +1,12 @@
 package dev.olog.owl.ui
 
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.font
 import androidx.compose.ui.text.font.fontFamily
 import androidx.compose.ui.unit.sp
 import dev.olog.owl.R
+import dev.olog.shared.utils.TypographyUtils
 
 
 val rubik = fontFamily(
@@ -15,7 +15,7 @@ val rubik = fontFamily(
     font(R.font.rubik_bold, FontWeight.Bold)
 )
 
-val typography = typographyFromDefaults(
+val typography = TypographyUtils.fromDefaults(
     h1 = TextStyle(
         fontFamily = rubik,
         fontWeight = FontWeight.Bold,
@@ -82,36 +82,3 @@ val typography = typographyFromDefaults(
         fontSize = 10.sp
     )
 )
-
-private fun typographyFromDefaults(
-    h1: TextStyle,
-    h2: TextStyle,
-    h3: TextStyle,
-    h4: TextStyle,
-    h5: TextStyle,
-    h6: TextStyle,
-    subtitle1: TextStyle,
-    subtitle2: TextStyle,
-    body1: TextStyle,
-    body2: TextStyle,
-    button: TextStyle,
-    caption: TextStyle,
-    overline: TextStyle
-): Typography {
-    val defaults = Typography()
-    return Typography(
-        h1 = defaults.h1.merge(h1),
-        h2 = defaults.h2.merge(h2),
-        h3 = defaults.h3.merge(h3),
-        h4 = defaults.h4.merge(h4),
-        h5 = defaults.h5.merge(h5),
-        h6 = defaults.h6.merge(h6),
-        subtitle1 = defaults.subtitle1.merge(subtitle1),
-        subtitle2 = defaults.subtitle2.merge(subtitle2),
-        body1 = defaults.body1.merge(body1),
-        body2 = defaults.body2.merge(body2),
-        button = defaults.button.merge(button),
-        caption = defaults.caption.merge(caption),
-        overline = defaults.overline.merge(overline)
-    )
-}
